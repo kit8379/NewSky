c968029345c15ed7244dc6d0e8c1fad788deb51e
Tony <manhhkit@gmail.com>
2024-03-03 01:54:54 +0800

    Update redis island pub sub class

--- src/main/java/org/me/newsky/island/IslandHandler.java
+++ src/main/java/org/me/newsky/island/IslandHandler.java
@@ -145,14 +145,6 @@ public abstract class IslandHandler {
         }
     }
 
-    public void subscribeToRequests() {
-        islandSubscribeRequest.subscribeToRequests();
-    }
-
-    public void unsubscribeFromRequests() {
-        islandSubscribeRequest.unsubscribeFromRequests();
-    }
-
     public abstract CompletableFuture<Void> loadIsland(UUID islandUuid);
 
     public abstract CompletableFuture<Void> teleportToIsland(UUID islandUuid, Player player, String locationString);
--- src/main/java/org/me/newsky/island/post/IslandPublishRequest.java
+++ src/main/java/org/me/newsky/island/post/IslandPublishRequest.java
@@ -9,68 +9,91 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.TimeUnit;
 
 public class IslandPublishRequest {
-    private static final long TIMEOUT_SECONDS = 30L;
     private final NewSky plugin;
     private final RedisHandler redisHandler;
     private final HeartBeatHandler heartBeatHandler;
     private final String serverID;
+    private static final ConcurrentHashMap<String, RequestData> pendingRequests = new ConcurrentHashMap<>();
 
-    public IslandPublishRequest(NewSky plugin, RedisHandler redisHandler, HeartBeatHandler heartBeatHandler, String serverID) {
+    public IslandPublishRequest(NewSky plugin, RedisHandler redisHandler, HeartBeatHandler heartBeatHandler,
+                                String serverID) {
         this.plugin = plugin;
         this.redisHandler = redisHandler;
         this.heartBeatHandler = heartBeatHandler;
         this.serverID = serverID;
+        subscribeToResponseChannel();
     }
 
-    public CompletableFuture<ConcurrentHashMap<String, String>> sendRequest(String targetServer, String operation) {
-        String requestID = "Req-" + UUID.randomUUID();
-        CompletableFuture<ConcurrentHashMap<String, String>> future = new CompletableFuture<>();
-        ConcurrentHashMap<String, String> responses = new ConcurrentHashMap<>();
-        Set<String> activeServers = heartBeatHandler.getActiveServers();
-
+    private void subscribeToResponseChannel() {
         JedisPubSub responseSubscriber = new JedisPubSub() {
             @Override
             public void onMessage(String channel, String message) {
-                String[] responseParts = message.split(":");
-                String responderID = responseParts[0];
-                String responseData = responseParts.length > 1 ? responseParts[1] : "";
+                String[] parts = message.split(":");
+                String messageType = parts[0];
+                String requestID = parts[1];
+                String responseServerID = parts[2];
+                String response = parts.length > 3 ? parts[3] : "";
 
-                responses.put(responderID, responseData);
-                plugin.debug("Received response from server: " + responderID + " for request: " + requestID + " for operation: " + operation + " with data: " + responseData);
+                if (messageType.equals("response")) {
+                    RequestData requestData = pendingRequests.get(requestID);
+                    if (requestData != null) {
+                        requestData.getResponses().put(responseServerID, response);
+                        plugin.debug("Received response from " + responseServerID + " for request " + requestID);
 
-                boolean shouldComplete = (targetServer.equals("all") && responses.keySet().containsAll(activeServers)) || responderID.equals(targetServer);
-
-                if (shouldComplete) {
-                    this.unsubscribe();
-                    if (responses.containsValue("Error")) {
-                        future.completeExceptionally(new IllegalStateException("Error received from server: " + responderID + " for request: " + requestID + " for operation: " + operation));
-                    } else {
-                        future.complete(responses);
+                        if (requestData.getExpectedResponses().equals(requestData.getResponses().keySet())) {
+                            requestData.getFuture().complete(requestData.getResponses());
+                            pendingRequests.remove(requestID);
+                            plugin.debug("Completed request " + requestID);
+                        }
                     }
                 }
             }
         };
+        redisHandler.subscribe(responseSubscriber, "newsky-response-channel");
+    }
 
-        redisHandler.subscribe(responseSubscriber, "newsky-response-channel-" + requestID);
-        String requestMessage = requestID + ":" + serverID + ":" + targetServer + ":" + operation;
+    public CompletableFuture<ConcurrentHashMap<String, String>> sendRequest(String targetServer, String operation) {
+        CompletableFuture<ConcurrentHashMap<String, String>> future = new CompletableFuture<>();
+        String requestID = UUID.randomUUID().toString();
+        Set<String> expectedResponses = targetServer.equals("all") ? heartBeatHandler.getActiveServers()
+                : Set.of(targetServer);
+        pendingRequests.put(requestID, new RequestData(future, expectedResponses));
 
+        String requestMessage = String.join(":", "request", requestID, serverID, targetServer, operation);
         redisHandler.publish("newsky-request-channel", requestMessage);
-        plugin.debug("Sending request: " + requestID + " to server: " + targetServer + " for operation: " + operation);
+        plugin.debug("Sent request " + requestID + " to " + targetServer + " with operation " + operation);
 
-        scheduleTimeoutTask(future, requestID, responseSubscriber);
+        future.orTimeout(30, TimeUnit.SECONDS).whenComplete((result, error) -> {
+            pendingRequests.remove(requestID);
+            plugin.debug("Timeout for request " + requestID);
+        });
 
         return future;
     }
 
-    private void scheduleTimeoutTask(CompletableFuture<ConcurrentHashMap<String, String>> future, String requestID, JedisPubSub responseSubscriber) {
-        plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
-            if (!future.isDone()) {
-                responseSubscriber.unsubscribe();
-                future.completeExceptionally(new TimeoutException("Timeout waiting for response to request: " + requestID));
-            }
-        }, TIMEOUT_SECONDS * 20);
+    private static class RequestData {
+        private final CompletableFuture<ConcurrentHashMap<String, String>> future;
+        private final Set<String> expectedResponses;
+        private final ConcurrentHashMap<String, String> responses = new ConcurrentHashMap<>();
+
+        public RequestData(CompletableFuture<ConcurrentHashMap<String, String>> future, Set<String> expectedResponses) {
+            this.future = future;
+            this.expectedResponses = expectedResponses;
+        }
+
+        public CompletableFuture<ConcurrentHashMap<String, String>> getFuture() {
+            return future;
+        }
+
+        public Set<String> getExpectedResponses() {
+            return expectedResponses;
+        }
+
+        public ConcurrentHashMap<String, String> getResponses() {
+            return responses;
+        }
     }
 }
