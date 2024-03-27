a774a3044ce9937ce891f014a3fe777fa7f93674
Tony <manhhkit@gmail.com>
2024-03-28 00:34:29 +0800

    Optimize code

--- src/main/java/org/me/newsky/cache/CacheHandler.java
+++ src/main/java/org/me/newsky/cache/CacheHandler.java
@@ -347,23 +347,4 @@ public class CacheHandler {
             return jedis.hget("island_server", islandUuid.toString());
         }
     }
-
-    public boolean acquireLock(String key, long expiration) {
-        try (Jedis jedis = redisHandler.getJedis()) {
-            String lockKey = "lock:" + key;
-            long result = jedis.setnx(lockKey, "locked");
-            if (result == 1) {
-                jedis.expire(lockKey, expiration);
-                return true;
-            }
-            return false;
-        }
-    }
-
-    public void releaseLock(String key) {
-        try (Jedis jedis = redisHandler.getJedis()) {
-            String lockKey = "lock:" + key;
-            jedis.del(lockKey);
-        }
-    }
 }
