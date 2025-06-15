193a26ea2b0c14ef56f79705081dfbcf22dbee0e
kit8379 <manhhkit@gmail.com>
2025-06-15 17:13:45 +0800

    Refactor cache handling to use RedisCache; update method names and improve documentation in various classes

---
+++ src/main/java/org/me/newsky/island/distributor/IslandDistributor.java
@@ -0,0 +1,235 @@
+package org.me.newsky.island.distributor;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.me.newsky.NewSky;
+import org.me.newsky.exceptions.IslandAlreadyLoadedException;
+import org.me.newsky.exceptions.IslandNotLoadedException;
+import org.me.newsky.exceptions.NoActiveServerException;
+import org.me.newsky.island.operation.IslandOperation;
+import org.me.newsky.broker.IslandBroker;
+import org.me.newsky.cache.RedisCache;
+import org.me.newsky.routing.ServerSelector;
+import org.me.newsky.util.ComponentUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+
+public class IslandDistributor {
+
+    private final NewSky plugin;
+    private final RedisCache redisCache;
+    private final IslandOperation islandOperation;
+    private final ServerSelector serverSelector;
+    private final String serverID;
+
+    private IslandBroker islandBroker;
+
+    public IslandDistributor(NewSky plugin, RedisCache redisCache, IslandOperation islandOperation, ServerSelector serverSelector, String serverID) {
+        this.plugin = plugin;
+        this.redisCache = redisCache;
+        this.islandOperation = islandOperation;
+        this.serverSelector = serverSelector;
+        this.serverID = serverID;
+    }
+
+    public void setIslandBroker(IslandBroker islandBroker) {
+        this.islandBroker = islandBroker;
+    }
+
+    public CompletableFuture<Void> createIsland(UUID islandUuid, UUID ownerUuid, String spawnLocation) {
+        String targetServer = selectServer();
+
+        if (targetServer == null) {
+            plugin.debug(getClass().getSimpleName(), "No active server found to handle island creation");
+            return CompletableFuture.failedFuture(new NoActiveServerException());
+        }
+
+        if (targetServer.equals(serverID)) {
+            plugin.debug(getClass().getSimpleName(), "Creating island in current server " + serverID);
+            return islandOperation.createIsland(islandUuid, ownerUuid, spawnLocation);
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Forwarding island creation request to server " + targetServer);
+            return islandBroker.sendRequest(targetServer, "create", islandUuid.toString(), ownerUuid.toString(), spawnLocation);
+        }
+    }
+
+    public CompletableFuture<Void> deleteIsland(UUID islandUuid) {
+        String islandServer = getServerByIsland(islandUuid);
+
+        if (islandServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is not currently loaded on any server");
+            plugin.debug(getClass().getSimpleName(), "Deleting island in current server " + serverID);
+            return islandOperation.deleteIsland(islandUuid);
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is currently on server: " + islandServer);
+            if (islandServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Deleting island in current server " + serverID);
+                return islandOperation.deleteIsland(islandUuid);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is on server " + islandServer + ", forwarding deletion request");
+                return islandBroker.sendRequest(islandServer, "delete", islandUuid.toString());
+            }
+        }
+    }
+
+    public CompletableFuture<Void> loadIsland(UUID islandUuid) {
+        String islandServer = getServerByIsland(islandUuid);
+
+        if (islandServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is not currently loaded on any server");
+            String targetServer = selectServer();
+            if (targetServer == null) {
+                plugin.debug(getClass().getSimpleName(), "No active server available to load island");
+                return CompletableFuture.failedFuture(new NoActiveServerException());
+            }
+
+            if (targetServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Loading island in current server " + serverID);
+                return islandOperation.loadIsland(islandUuid);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding load request to server " + targetServer);
+                return islandBroker.sendRequest(targetServer, "load", islandUuid.toString());
+            }
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is already loaded on server: " + islandServer);
+            return CompletableFuture.failedFuture(new IslandAlreadyLoadedException());
+        }
+    }
+
+    public CompletableFuture<Void> unloadIsland(UUID islandUuid) {
+        String islandServer = getServerByIsland(islandUuid);
+
+        if (islandServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is not currently loaded on any server");
+            return CompletableFuture.failedFuture(new IslandNotLoadedException());
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is currently on server: " + islandServer);
+            if (islandServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Unloading island in current server " + serverID);
+                return islandOperation.unloadIsland(islandUuid);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding unload request to server " + islandServer);
+                return islandBroker.sendRequest(islandServer, "unload", islandUuid.toString());
+            }
+        }
+    }
+
+    public void lockIsland(UUID islandUuid) {
+        String islandServer = getServerByIsland(islandUuid);
+
+        if (islandServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is not currently loaded on any server");
+            CompletableFuture.completedFuture(null);
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is currently on server: " + islandServer);
+            if (islandServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Locking island in current server " + serverID);
+                islandOperation.lockIsland(islandUuid);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding lock request to server " + islandServer);
+                islandBroker.sendRequest(islandServer, "lock", islandUuid.toString());
+            }
+        }
+    }
+
+    public CompletableFuture<Void> teleportIsland(UUID islandUuid, UUID playerUuid, String teleportLocation) {
+        String islandServer = getServerByIsland(islandUuid);
+
+        if (islandServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is not currently loaded on any server");
+            String targetServer = selectServer();
+            if (targetServer == null) {
+                plugin.debug(getClass().getSimpleName(), "No active server available for teleportation");
+                return CompletableFuture.failedFuture(new NoActiveServerException());
+            }
+
+            if (targetServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Teleporting in current server " + serverID);
+                return islandOperation.teleportIsland(islandUuid, playerUuid, teleportLocation);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding teleport request to server " + targetServer);
+                return islandBroker.sendRequest(targetServer, "teleport", islandUuid.toString(), playerUuid.toString(), teleportLocation).thenRun(() -> connectToServer(playerUuid, targetServer));
+            }
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is currently on server: " + islandServer);
+            if (islandServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Teleporting in current server " + serverID);
+                return islandOperation.teleportIsland(islandUuid, playerUuid, teleportLocation);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding teleport request to server " + islandServer);
+                return islandBroker.sendRequest(islandServer, "teleport", islandUuid.toString(), playerUuid.toString(), teleportLocation).thenRun(() -> connectToServer(playerUuid, islandServer));
+            }
+        }
+    }
+
+    public void expelPlayer(UUID islandUuid, UUID playerUuid) {
+        String islandServer = getServerByIsland(islandUuid);
+
+        if (islandServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is not currently loaded on any server");
+            CompletableFuture.completedFuture(null);
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Island " + islandUuid + " is currently on server: " + islandServer);
+            if (islandServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Expelling player in current server " + serverID);
+                islandOperation.expelPlayer(islandUuid, playerUuid);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding expel request to server " + islandServer);
+                islandBroker.sendRequest(islandServer, "expel", islandUuid.toString(), playerUuid.toString());
+            }
+        }
+    }
+
+    public void sendPlayerMessage(UUID playerUuid, Component message) {
+        String playerName = Bukkit.getOfflinePlayer(playerUuid).getName();
+
+        String playerServer = getServerByPlayer(playerName);
+
+        if (playerServer == null) {
+            plugin.debug(getClass().getSimpleName(), "Player is not currently online on any server");
+        } else {
+            plugin.debug(getClass().getSimpleName(), "Player " + playerName + " is currently on server: " + playerServer);
+            if (playerServer.equals(serverID)) {
+                plugin.debug(getClass().getSimpleName(), "Sending message to player on current server " + serverID);
+                islandOperation.sendPlayerMessage(playerUuid, message);
+            } else {
+                plugin.debug(getClass().getSimpleName(), "Forwarding message to player on server " + playerServer);
+                islandBroker.sendRequest(playerServer, "message", playerUuid.toString(), ComponentUtils.serialize(message));
+            }
+        }
+    }
+
+    private String selectServer() {
+        Map<String, String> activeServers = redisCache.getActiveGameServers();
+        return serverSelector.selectServer(activeServers);
+    }
+
+    private String getServerByIsland(UUID islandUuid) {
+        return redisCache.getIslandLoadedServer(islandUuid).orElse(null);
+    }
+
+    public String getServerByPlayer(String playerName) {
+        return redisCache.getOnlinePlayerServer(playerName).orElse(null);
+    }
+
+    private void connectToServer(UUID playerUuid, String serverName) {
+        Player player = Bukkit.getPlayer(playerUuid);
+        if (player != null) {
+            try (ByteArrayOutputStream byteArray = new ByteArrayOutputStream(); DataOutputStream out = new DataOutputStream(byteArray)) {
+                out.writeUTF("Connect");
+                out.writeUTF(serverName);
+                player.sendPluginMessage(plugin, "BungeeCord", byteArray.toByteArray());
+                plugin.debug(getClass().getSimpleName(), "Sent plugin message to connect player " + player.getName() + " to server " + serverName);
+            } catch (IOException e) {
+                plugin.getLogger().log(Level.SEVERE, "Failed to send plugin message to player " + player.getName(), e);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/me/newsky/island/operation/IslandOperation.java
@@ -0,0 +1,145 @@
+package org.me.newsky.island.operation;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.me.newsky.NewSky;
+import org.me.newsky.cache.Cache;
+import org.me.newsky.config.ConfigHandler;
+import org.me.newsky.cache.RedisCache;
+import org.me.newsky.teleport.TeleportHandler;
+import org.me.newsky.util.IslandUtils;
+import org.me.newsky.util.LocationUtils;
+import org.me.newsky.world.WorldHandler;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Handles post-island operations such as creating, deleting, loading, unloading, and locking islands.
+ * This class is responsible for performing these operations on the server where the actual operation is to be performed.
+ */
+public class IslandOperation {
+
+    private final NewSky plugin;
+    private final ConfigHandler config;
+    private final Cache cache;
+    private final RedisCache redisCache;
+    private final WorldHandler worldHandler;
+    private final TeleportHandler teleportHandler;
+    private final String serverID;
+
+    public IslandOperation(NewSky plugin, ConfigHandler config, Cache cache, RedisCache redisCache, WorldHandler worldHandler, TeleportHandler teleportHandler, String serverID) {
+        this.plugin = plugin;
+        this.config = config;
+        this.cache = cache;
+        this.redisCache = redisCache;
+        this.worldHandler = worldHandler;
+        this.teleportHandler = teleportHandler;
+        this.serverID = serverID;
+    }
+
+    public CompletableFuture<Void> createIsland(UUID islandUuid, UUID ownerUuid, String spawnLocation) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        return worldHandler.createWorld(islandName).thenRun(() -> {
+            redisCache.updateIslandLoadedServer(islandUuid, serverID);
+            cache.createIsland(islandUuid);
+            cache.updateIslandPlayer(islandUuid, ownerUuid, "owner");
+            cache.updateHomePoint(islandUuid, ownerUuid, "default", spawnLocation);
+            plugin.debug(getClass().getSimpleName(), "Island " + islandName + " created successfully with owner " + ownerUuid);
+        });
+    }
+
+    public CompletableFuture<Void> deleteIsland(UUID islandUuid) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        return worldHandler.deleteWorld(islandName).thenRun(() -> {
+            redisCache.removeIslandLoadedServer(islandUuid);
+            cache.deleteIsland(islandUuid);
+            plugin.debug(getClass().getSimpleName(), "Island " + islandName + " deleted successfully");
+        });
+    }
+
+    public CompletableFuture<Void> loadIsland(UUID islandUuid) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        return worldHandler.loadWorld(islandName).thenRun(() -> {
+            redisCache.updateIslandLoadedServer(islandUuid, serverID);
+            plugin.debug(getClass().getSimpleName(), "Island " + islandName + " loaded on server " + serverID);
+        });
+    }
+
+    public CompletableFuture<Void> unloadIsland(UUID islandUuid) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        return worldHandler.unloadWorld(islandName).thenRun(() -> {
+            redisCache.removeIslandLoadedServer(islandUuid);
+            plugin.debug(getClass().getSimpleName(), "Island " + islandName + " unloaded from server " + serverID);
+        });
+    }
+
+    public CompletableFuture<Void> lockIsland(UUID islandUuid) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        World world = Bukkit.getWorld(islandName);
+        if (world != null) {
+            Set<UUID> islandPlayers = cache.getIslandPlayers(islandUuid);
+            for (Player player : world.getPlayers()) {
+                UUID playerUuid = player.getUniqueId();
+                if (!islandPlayers.contains(playerUuid)) {
+                    player.teleportAsync(Bukkit.getServer().getWorlds().getFirst().getSpawnLocation());
+                    Bukkit.getScheduler().runTask(plugin, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), config.getLobbyCommand(player.getName())));
+                    plugin.debug(getClass().getSimpleName(), "Player " + playerUuid + " teleported to safe location due to island lock");
+                }
+            }
+        }
+
+        return CompletableFuture.completedFuture(null);
+    }
+
+    public CompletableFuture<Void> teleportIsland(UUID islandUuid, UUID playerUuid, String teleportLocation) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        return loadIsland(islandUuid).thenRun(() -> {
+            Location location = LocationUtils.stringToLocation(islandName, teleportLocation);
+            Player player = Bukkit.getPlayer(playerUuid);
+            if (player != null) {
+                player.teleportAsync(location);
+                plugin.debug(getClass().getSimpleName(), "Player " + playerUuid + " teleported to island " + islandUuid + " at location: " + teleportLocation);
+            } else {
+                teleportHandler.addPendingTeleport(playerUuid, location);
+                plugin.debug(getClass().getSimpleName(), "Player " + playerUuid + " is offline, teleport will be processed when they log in");
+            }
+        });
+    }
+
+    public CompletableFuture<Void> expelPlayer(UUID islandUuid, UUID playerUuid) {
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+
+        World world = Bukkit.getWorld(islandName);
+        if (world != null) {
+            Player player = Bukkit.getPlayer(playerUuid);
+            if (player != null) {
+                player.teleportAsync(Bukkit.getServer().getWorlds().getFirst().getSpawnLocation());
+                Bukkit.getScheduler().runTask(plugin, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), config.getLobbyCommand(player.getName())));
+                plugin.debug(getClass().getSimpleName(), "Player " + playerUuid + " expelled from island " + islandName);
+            }
+        }
+
+        return CompletableFuture.completedFuture(null);
+    }
+
+    public CompletableFuture<Void> sendPlayerMessage(UUID playerUuid, Component message) {
+        Player player = Bukkit.getPlayer(playerUuid);
+        if (player != null) {
+            player.sendMessage(message);
+            plugin.debug(getClass().getSimpleName(), "Sent message to player " + playerUuid);
+        }
+
+        return CompletableFuture.completedFuture(null);
+    }
+}
