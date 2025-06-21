9acf16cbfadfe50d28a0967779c62a9b67c99b58
kit8379 <manhhkit@gmail.com>
2025-06-22 04:23:18 +0800

    Refactor island level calculation; optimize chunk snapshot retrieval and processing

--- src/main/java/org/me/newsky/island/LevelHandler.java
+++ src/main/java/org/me/newsky/island/LevelHandler.java
@@ -1,15 +1,15 @@
 package org.me.newsky.island;
 
-import org.bukkit.Chunk;
+import org.bukkit.ChunkSnapshot;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
+import org.bukkit.World;
 import org.me.newsky.NewSky;
 import org.me.newsky.cache.Cache;
 import org.me.newsky.config.ConfigHandler;
-import org.me.newsky.util.ChunkUtils;
 import org.me.newsky.util.IslandUtils;
 import org.me.newsky.util.LocationUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -28,33 +28,57 @@ public class LevelHandler {
     }
 
     public void calIslandLevel(UUID islandUuid) {
-        CompletableFuture.runAsync(() -> {
-            String islandName = IslandUtils.UUIDToName(islandUuid);
+        String islandName = IslandUtils.UUIDToName(islandUuid);
+        Location center = LocationUtils.stringToLocation(islandName, config.getIslandSpawnX() + "," + config.getIslandSpawnY() + "," + config.getIslandSpawnZ() + "," + config.getIslandSpawnYaw() + "," + config.getIslandSpawnPitch());
 
-            Location center = LocationUtils.stringToLocation(islandName, config.getIslandSpawnX() + "," + config.getIslandSpawnY() + "," + config.getIslandSpawnZ() + "," + config.getIslandSpawnYaw() + "," + config.getIslandSpawnPitch());
+        int size = config.getIslandSize();
+        int halfSize = size / 2;
 
-            int size = config.getIslandSize();
-            List<Chunk> chunks = ChunkUtils.getChunksForIsland(center, size);
+        int minX = (center.getBlockX() - halfSize) >> 4;
+        int minZ = (center.getBlockZ() - halfSize) >> 4;
+        int maxX = (center.getBlockX() + halfSize) >> 4;
+        int maxZ = (center.getBlockZ() + halfSize) >> 4;
 
-            int totalPoints = chunks.parallelStream().mapToInt(chunk -> {
+        World world = center.getWorld();
+
+        List<CompletableFuture<ChunkSnapshot>> snapshotFutures = new ArrayList<>();
+
+        for (int x = minX; x <= maxX; x++) {
+            for (int z = minZ; z <= maxZ; z++) {
+                CompletableFuture<ChunkSnapshot> future = world.getChunkAtAsync(x, z, true).thenCompose(chunk -> {
+                    if (chunk == null) {
+                        return CompletableFuture.completedFuture(null);
+                    }
+                    CompletableFuture<ChunkSnapshot> snapshotFuture = new CompletableFuture<>();
+                    plugin.getServer().getScheduler().runTask(plugin, () -> snapshotFuture.complete(chunk.getChunkSnapshot()));
+                    return snapshotFuture;
+                });
+                snapshotFutures.add(future);
+            }
+        }
+
+        CompletableFuture.allOf(snapshotFutures.toArray(new CompletableFuture[0])).thenApply(v -> snapshotFutures.stream().map(CompletableFuture::join).filter(snapshot -> snapshot != null).toList()).thenApplyAsync(snapshots -> {
+            int minY = world.getMinHeight();
+            int maxY = world.getMaxHeight();
+
+            return snapshots.parallelStream().mapToInt(snapshot -> {
                 int points = 0;
                 for (int x = 0; x < 16; x++) {
                     for (int z = 0; z < 16; z++) {
-                        for (int y = 0; y < chunk.getWorld().getMaxHeight(); y++) {
-                            Block block = chunk.getBlock(x, y, z);
-                            int value = config.getBlockLevel(block.getType().name());
+                        for (int y = minY; y < maxY; y++) {
+                            String typeName = snapshot.getBlockType(x, y, z).name();
+                            int value = config.getBlockLevel(typeName);
                             points += value;
                         }
                     }
                 }
                 return points;
             }).sum();
-
+        }, plugin.getBukkitAsyncExecutor()).thenAccept(totalPoints -> {
             int totalLevel = (int) Math.round((double) totalPoints / 100);
-
             cache.updateIslandLevel(islandUuid, totalLevel);
             plugin.debug("LevelHandler", "Calculated level for island " + islandUuid + ": " + totalLevel);
-        }, plugin.getBukkitAsyncExecutor());
+        });
     }
 
     public int getIslandLevel(UUID islandUuid) {
